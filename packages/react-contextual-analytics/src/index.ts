c44e3564e04148b96d3ddb47bb910f789d7d84cc
Alexander Forbes-Reed <git@alex.forbes.red>
2025-10-15 12:52:04 +0200

    added missing exports

--- packages/react-contextual-analytics/src/index.ts
+++ packages/react-contextual-analytics/src/index.ts
@@ -1,4 +1,5 @@
-export { useAnalytics } from './use-analytics';
+export { useAnalytics, type AnalyticsEmitter } from './use-analytics';
+export type { Map, PropsWithEmitterChild, Provider } from "./types";
 export { AnalyticsBoundaryProvider } from './boundary-provider';
 export { AnalyticsProvider } from './analytics-provider';
-export type { AnalyticsClient } from './client';
+export { type AnalyticsClient, createAnalyticsClient } from './client';
